259fed0bb94afe57f8d485dac7a93602ee70aeaf
Shakthi <shakthiraj577@gmail.com>
2023-06-23 22:33:27 +0530

    clone

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,12 +7,12 @@ import { demoChannelUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle } from "
 const VideoCard = ({ video: {id: {videoId}, snippet} }) => {
     // console.log(videoId, snippet);
   return (
-    <Card sx={{ width: { md: '316px',xs: '100%' }, boxShadow: 'none', borderRadius: 0}} >
+    <Card sx={{ width: { xs: '100%', sm:'100%', md: '316px' }, boxShadow: 'none', borderRadius: 0}} >
         <Link to={videoId ? `/video/${videoId}`: demoVideoUrl }>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url}
                 alt={snippet?.title}
-                sx={{ width: 356, height: 180}} />
+                sx={{ width: { xs: '100%', sm: '100%', md: '316px'}, height: 180}} />
         </Link>
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '80px'}} >
             <Link to={videoId ? `/video/${videoId}`: demoVideoUrl }>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,12 @@
 import { Stack, Box } from '@mui/material'
 import { VideoCard,ChannelCard } from './'
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   // console.log(videos);
+  if(!videos?.length) return 'Loading...'
 
   return (
-    <Stack direction="row" gap={2} flexWrap="wrap" justifyContent="start">
+    <Stack direction={direction || "row" } gap={2} flexWrap="wrap" justifyContent="start">
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
